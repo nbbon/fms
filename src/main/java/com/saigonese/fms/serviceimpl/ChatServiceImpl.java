8f704276d213d3997682511d78d5314ce27a6132
Bon Nguyen <ngbabon@gmail.com>
2018-10-26 07:55:46 -0500

    revert ChatMessage

--- src/main/java/com/saigonese/fms/serviceimpl/ChatServiceImpl.java
+++ src/main/java/com/saigonese/fms/serviceimpl/ChatServiceImpl.java
@@ -1,6 +1,5 @@
 package com.saigonese.fms.serviceimpl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.saigonese.fms.domain.ChatMessage;
@@ -9,8 +8,7 @@ import com.saigonese.fms.service.ChatService;
 @Service
 public class ChatServiceImpl implements ChatService {
 	
-	@Autowired
-	private ChatMessage lastMessage;
+	private ChatMessage lastMessage = new ChatMessage();
 	
 	@Override
 	public ChatMessage getLastMessage() {
